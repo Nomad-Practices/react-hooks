b64de0878d59dec72134c129979ed65c730251ff
sjk <jkshin0903@gmail.com>
2022-02-13 18:39:22 +0900

    custom hooks
    using useState hook

---
+++ hooks/useInput.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(event.target.value);
+    }
+  };
+  return {
+    value,
+    onChange,
+  };
+};
+
+export default useInput;
+// Use Case~
+// export default function App() {
+//   const maxLen = (value) => !value.includes("@");
+//   const name = useInput("Mr.", maxLen);
+//   return (
+//     <div className="App">
+//       <h1>Hello</h1>
+//       <input placeholder="Name" {...name} />
+//     </div>
+//   );
+// }
---
+++ hooks/useTabs.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
+
+export default useTabs;
+// Use case~
+// export default function App() {
+//   const { currentItem, changeItem } = useTabs(0, content);
+//   return (
+//     <div className="App">
+//       {content.map((section, index) => (
+//         <button onClick={() => changeItem(index)}>{section.tab}</button>
+//       ))}
+//       <div>{currentItem.content}</div>
+//     </div>
+//   );
+// }
