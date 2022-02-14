155ee4223d3d10cb22e534776bda83c3bc858f67
sjk <jkshin0903@gmail.com>
2022-02-14 23:14:55 +0900

    custom hook
    useClick : useRef가 잘 이해되지 않는다...

---
+++ hooks/useClick.js
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+  });
+  return element;
+};
+
+export default useClick;
+// Use cases~
+// export default function App() {
+//   const onClick = () => {
+//     console.log("say hello");
+//   };
+//   const title = useClick(onClick);
+//   return (
+//     <div className="App">
+//       <h1 ref={title}>hi</h1>
+//     </div>
+//   );
+// }
