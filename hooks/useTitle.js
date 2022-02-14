1c6b68269d1555c3f75ae41cff091c4fa28240b8
sjk <jkshin0903@gmail.com>
2022-02-14 22:45:52 +0900

    useTitle 설명추가

--- hooks/useTitle.js
+++ hooks/useTitle.js
@@ -17,3 +17,6 @@ export default useTitle;
 //   setTimeout(() => titleUpdater("im changed"), 5000);
 //   return <div className="App"></div>;
 // }
+// App 컴포넌트가 mounted되면서 useEffect가 한번 실행한다.
+// 5초 뒤 setTitle 함수를 비동기적으로 호출하면 상위 context에 있는 title에 변화를 주고
+// useEffect는 동일한 context에 위치한 dependency change에 의해서 updateTitle을 한번 더 실행한다.
