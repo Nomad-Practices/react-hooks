6df92969ed287e1cb0c07b9c1dee94290d8f3e2d
sjk <jkshin0903@gmail.com>
2022-02-15 22:42:03 +0900

    custom hook
    useFadeIn ==> 이를 응용하여 Vue에서 useImageLazy도 만들 수 있다!!

---
+++ hooks/useFadeIn.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+import { useEffect, useRef } from "react";
+
+const useFadeIn = (duration = 1, delay = 0) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
+  }, [duration, delay]);
+  return {
+    ref: element,
+    style: { opacity: 0 },
+  };
+};
+// Use Case~
+// export default function App() {
+//   const fadeInH1 = useFadeIn(1, 2);
+//   const fadeInP = useFadeIn(5, 10);
+//   return (
+//     <div className="App">
+//       <h1 {...fadeInH1}>Hello</h1>
+//       <p {...fadeInP}>lalalalala</p>
+//     </div>
+//   );
+// }
+// ==> useImageLazy hook도 한번 만들어보자~
