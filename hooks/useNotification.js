befbecd77cb9cd3717e0e651e17575de70247874
sjk <jkshin0903@gmail.com>
2022-02-16 22:04:28 +0900

    custom hook
    useNotification~

---
+++ hooks/useNotification.js
@@ -0,0 +1,30 @@
+const useNotification = (title, options) => {
+  if (!"Notification" in window) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
+  return fireNotif;
+};
+// 브라우저에 내장된 알림기능을 hook을 통해 구현할 수 있다.
+// export default function App() {
+//   const triggerNotif = useNotification("say hello~", {
+//     body: "i love it",
+//   });
+//   return (
+//     <div className="App">
+//       <button onClick={triggerNotif}>noti</button>
+//     </div>
+//   );
+// }
